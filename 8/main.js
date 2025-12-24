2be48711970a56e5c897a38816484830a6355713
Ster3ak <foxproxy8@gmail.com>
2025-12-24 21:05:19 +0300

    практика 8

---
+++ 8/main.js
@@ -0,0 +1,35 @@
+async function getUsers() {
+    try {
+     const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+     if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}`);
+     }
+    const users = await response.json();
+    console.log(`Список пользователей: `);
+      users.forEach(user => {
+       console.log(user.name);
+      });
+      return users;
+        } catch (error) {
+          console.error(`Операция не удалась: ${error.message}`);
+        }
+      }
+      async function getPost(id) {
+        try {
+          let url = `https://jsonplaceholder.typicode.com/posts`;
+          if (id !== undefined) {
+            url += `/${id}`;
+          }
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(`Ошибка HTTP: ${response.status}`);
+          }
+          const post = await response.json();
+          return post;
+        } catch (error) {
+          console.error(`Ошибка: ${error.message}`);
+        }
+      }
+      getUsers();
+      getPost(1).then(post => console.log(post));
+      getPost(5).then(post => console.log(post));
